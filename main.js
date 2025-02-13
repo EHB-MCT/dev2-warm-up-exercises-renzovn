5f4158e65c573de494ea6be290b29310d44bdcfc
Renzo Vandingenen <renzo.vandingenen@student.ehb.be>
2025-02-13 16:09:49 +0100

    warmup1

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-import {
-    getRandomScores,
-    getStudentJSON,
-    runTitle
-} from './lib.js';
+import { getRandomScores, getStudentJSON, runTitle } from "./lib.js";
 
 runTitle();
+warmup1();
 
 function warmup1() {
-    console.log("Exercise week 1");
+  console.log("Exercise week 1");
+  const button = document.querySelector("#button-1a");
+  button.addEventListener("click", function () {
+    console.log("click");
+    document.querySelector("#content-1").innerHTML =
+      "<p>Hallo, dit is de oefening van Renzo</p>";
+  });
 }
 
 function warmup2() {
-    console.log("Exercise week 2");
-    const scores = getRandomScores(10);
+  console.log("Exercise week 2");
+  const scores = getRandomScores(10);
 }
 
 function warmup3() {
-    let student = getStudentJSON();
+  let student = getStudentJSON();
 }
 
 function warmup4() {
-    let main, min, max;
+  let main, min, max;
 }
 
-function warmup5() {
-
-}
+function warmup5() {}
